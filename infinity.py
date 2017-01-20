195c506648eaebaa466f4a4fb07df1e58e23655e
yn-coder <yn.coder@gmail.com>
2017-01-20 16:18:56 +0300

    Simple draft with pure layout #2

--- infinity.py
+++ infinity.py
@@ -2,7 +2,7 @@ from bottle import route, run, template, error
 
 @route('/')
 def index():
-    return '<h1>Infinite site</h1><p><a href="1">1</a></p>'
+    return '<h1>Infinite site</h1><p>Start at <a href="1">1</a></p>'
 
 @error(404)
 def error404(error):
@@ -14,11 +14,11 @@ def index(code):
         i = int(code)
         next = i + 1
         #s = '%i %i' % (i,i)
-        s = '<h1>Code: %i</h1><p><a href="%i">%i</a></p>' % (i, next, next )
+        s = template('<h1>Code: {{i}}</h1><p><a href="https://www.google.com/search?q={{i}}">Some interesting about {{i}} in Google</a></p><p><a href="https://www.wolframalpha.com/input/?i={{i}}">Some interesting about {{i}} on WolframAlpha</a></p><h1>Next</h1><p><a href="{{next}}">Next {{next}}</a></p>', i = i, next = next )
     except:
         #abort(404)
         s = template('{{code}} not an integer', code=code)
 
-    return s + '<p><a href="/">Index</a></p>'
+    return s + '<hr><p><a href="/">Index</a></p>'
 
 run(host='localhost', port=8080, reloader=True)
