5ee795444d33867bb89ee602c3578b6b7771e518
yn-coder <yn.coder@gmail.com>
2017-01-24 17:57:28 +0300

    wiki url #5

--- infinity.py
+++ infinity.py
@@ -10,12 +10,22 @@ def index():
 def error404(error):
     return 'Nothing here, sorry' + FOOTER_HTML
     
+GOOGLE_URL = '<p><a href="https://www.google.com/search?q={{i}}">Some interesting about {{i}} in Google</a></p>'
+WOLFRAM_ALPHA_URL = '<p><a href="https://www.wolframalpha.com/input/?i={{i}}">Some interesting about {{i}} on WolframAlpha</a></p>'
+DUCKDUCKGO_URL = '<p><a href="https://duckduckgo.com/?q={{i}}">Some interesting about {{i}} in DuckDuckGo</a></p>'
+WIKI_URL = '<p><a href="https://en.wikipedia.org/w/index.php?search={{i}}">Some interesting about {{i}} in Wikipedia</a>'
+
 @route('/<code>')
 def pages(code):
     try:
         i = int(code)
         next = i + 1
-        s = template('<h1>Code: {{i}}</h1><p><a href="https://www.google.com/search?q={{i}}">Some interesting about {{i}} in Google</a></p><p><a href="https://www.wolframalpha.com/input/?i={{i}}">Some interesting about {{i}} on WolframAlpha</a></p><p><a href="https://duckduckgo.com/?q={{i}}">Some interesting about {{i}} in DuckDuckGo</a></p><h1>Next</h1><p><a href="{{next}}">Next {{next}}</a></p>', i = i, next = next )
+        s = template('<h1>Code: {{i}}</h1>' +
+                     GOOGLE_URL +
+                     WOLFRAM_ALPHA_URL +                     
+                     DUCKDUCKGO_URL +
+                     WIKI_URL +
+                     '<h1>Next</h1><p><a href="{{next}}">Next {{next}}</a></p>', i = i, next = next )        
     except:
         #abort(404)
         s = template('{{code}} not an integer', code=code)
