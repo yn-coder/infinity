cc0831aede7272eb92c16388f2e4e83f8e4d4acd
yn-coder <yn.coder@gmail.com>
2017-01-25 12:14:34 +0300

    refactoring for #7

--- infinity.py
+++ infinity.py
@@ -5,30 +5,35 @@ def format_link( s_url, s_name ):
 
 FOOTER_HTML = '<hr><p>' + format_link( '/', 'Index' ) + '</p><p>' + format_link( 'https://github.com/yn-coder/infinity', 'Source code on GitHub' ) + '</p>'
 
+# index page
 @route('/')
 def index():
     return '<h1>Infinite site</h1><p>Start ' + format_link( '1', 'at 1' ) + '</p>' + FOOTER_HTML
 
+# 404 page
 @error(404)
 def error404(error):
     return 'Nothing here, sorry' + FOOTER_HTML
 
+# List of internet urls for integer. Add new url here
 URL_LIST = ( ( 'https://www.google.com/search?q={{i}}' , 'in Google' ),
              ( 'https://www.wolframalpha.com/input/?i={{i}}' , 'on WolframAlpha' ),
              ( 'https://duckduckgo.com/?q={{i}}' , 'in DuckDuckGo' ),
              ( 'https://en.wikipedia.org/w/index.php?search={{i}}' , 'in Wikipedia' ),
  )
-
+ 
+# page for integers
 @route('/<code>')
 def pages(code):
     try:
         i = int(code)
         next = i + 1
         
+        # format list of links
         links = ''
-        for uu in URL_LIST:
-            links = links + '<li>' + format_link( uu[0], uu[1] ) + '</li>'
-        
+        for ue in URL_LIST:
+            links = links + '<li>' + format_link( ue[0], ue[1] ) + '</li>'
+        # format page
         s = template('<h1>Code: {{i}}</h1>' +
                      '<p>Something interesting about <strong>{{i}}</strong>:</p><ul>' +
                      links +
