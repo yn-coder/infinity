7efcd443d415933a23e8881d40f7db66400d5435
yn-coder <yn.coder@gmail.com>
2017-01-25 12:08:44 +0300

    Refactoring for list of links and urls #7

--- infinity.py
+++ infinity.py
@@ -1,33 +1,39 @@
 from bottle import route, run, template, error, default_app
 
-FOOTER_HTML = '<hr><p><a href="/">Index</a></p><p><a href="https://github.com/yn-coder/infinity">Source code on GitHub</a></p>'
+def format_link( s_url, s_name ):
+    return '<a href="' + s_url + '">' + s_name + '</a>'
+
+FOOTER_HTML = '<hr><p>' + format_link( '/', 'Index' ) + '</p><p>' + format_link( 'https://github.com/yn-coder/infinity', 'Source code on GitHub' ) + '</p>'
 
 @route('/')
 def index():
-    return '<h1>Infinite site</h1><p>Start <a href="1">at 1</a></p>' + FOOTER_HTML
+    return '<h1>Infinite site</h1><p>Start ' + format_link( '1', 'at 1' ) + '</p>' + FOOTER_HTML
 
 @error(404)
 def error404(error):
     return 'Nothing here, sorry' + FOOTER_HTML
-    
-GOOGLE_URL = '<li><a href="https://www.google.com/search?q={{i}}">in Google</a></li>'
-WOLFRAM_ALPHA_URL = '<li><a href="https://www.wolframalpha.com/input/?i={{i}}">on WolframAlpha</a></li>'
-DUCKDUCKGO_URL = '<li><a href="https://duckduckgo.com/?q={{i}}">in DuckDuckGo</a></li>'
-WIKI_URL = '<li><a href="https://en.wikipedia.org/w/index.php?search={{i}}">in Wikipedia</a></li>'
+
+URL_LIST = ( ( 'https://www.google.com/search?q={{i}}' , 'in Google' ),
+             ( 'https://www.wolframalpha.com/input/?i={{i}}' , 'on WolframAlpha' ),
+             ( 'https://duckduckgo.com/?q={{i}}' , 'in DuckDuckGo' ),
+             ( 'https://en.wikipedia.org/w/index.php?search={{i}}' , 'in Wikipedia' ),
+ )
 
 @route('/<code>')
 def pages(code):
     try:
         i = int(code)
         next = i + 1
+        
+        links = ''
+        for uu in URL_LIST:
+            links = links + '<li>' + format_link( uu[0], uu[1] ) + '</li>'
+        
         s = template('<h1>Code: {{i}}</h1>' +
                      '<p>Something interesting about <strong>{{i}}</strong>:</p><ul>' +
-                     GOOGLE_URL +
-                     WOLFRAM_ALPHA_URL +                     
-                     DUCKDUCKGO_URL +
-                     WIKI_URL +
+                     links +
                      '</ul>' +
-                     '<h1>Next</h1><p><a href="{{next}}">Next {{next}}</a></p>', i = i, next = next )        
+                     '<h1>Next</h1><p>' + format_link( '{{next}}', 'Next {{next}}' ) + '</p>', i = i, next = next )
     except:
         #abort(404)
         s = template('{{code}} not an integer', code=code)
